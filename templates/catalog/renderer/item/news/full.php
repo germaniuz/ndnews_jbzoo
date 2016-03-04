b80dd9852d21f0f91ce6d924a29fc820ec9eb1cf
germaniuz <germanius@list.ru>
2016-03-04 14:59:38 +0300

    Add blank full template for news item

---
+++ templates/catalog/renderer/item/news/full.php
@@ -0,0 +1,15 @@
+<?
+/*
+ * @package           jbzoo
+ * @version           2.x.Pro
+ * @template_author   German Martirosyan http://www.studio-aw.ru
+ * @item               news    
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+$align  = $this->app->jbitem->getMediaAlign($item, $layout);
+$tabsId = $this->app->jbstring->getId('tabs');
+?>
+
